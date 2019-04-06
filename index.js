eb36ed08c9ad36f37e4b7bf05a5f456b91e797f8
MJaik <48388758+MJaik@users.noreply.github.com>
2019-04-06 13:35:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 
 const bot = Discord.Client({disableEveryone = true});
+
+bot.on("ready", async () => {
+  bot.user.setActivity("Привет!", {type: "STREAMING"});
+})
