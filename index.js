b2d3a9ec05eae823b4080230fe75c415435b73dd
MJaik <48388758+MJaik@users.noreply.github.com>
2019-04-08 16:15:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
+let prefix = %;
 
 const bot = Discord.Client;
 
 bot.login(process.env.BOT_TOKEN)
 
 bot.on("ready", async () => {
-  bot.user.setActivity("Привет!", {type: "STREAMING"});
+  bot.user.setActivity('Привет!', {type: 'STREAMING'});
 })
 
 bot.on("message", async message => {
   if(message.author.bot || message.channel.type = "dm") return;
   
-  let prefix = %;
-  
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
   
-  if(cmd === '%hello') {
-    return message.channel.send("Привет!");
-  }
+  switch(messageArray[0]) {
+    case prefix + "hello":
+        message.channel.send("Привет!")
+      break;
 })
 
 bot.login(botconfig.token);
